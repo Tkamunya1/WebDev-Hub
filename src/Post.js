d8229b617fca259f8d4ea96b8ba12b31203cb72e
Titus <titus.kamunya@student.moringaschool.com>
2022-07-28 14:20:07 +0300

    this commit will validate data on the form

--- src/Post.js
+++ src/Post.js
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
+
 const Post = () => {
+    const [title, setTitle ]= useState ("") 
+    const [body, setBody ]= useState ("") 
+    const [author, setAuthor ]= useState ("") 
+
     return (
         <div className = "post-book">
             <h1> Post a New Book </h1>
@@ -7,16 +14,22 @@ const Post = () => {
                 <input
                  type="text" 
                  required
+                 value={title}
+                 onChange={(e) => setTitle(e.target.value)}
                  />
                  <label> Book Body  </label>
                  <textarea 
                  required
+                 value={body}
+          onChange={(e) => setBody(e.target.value)}
                  >
                  </textarea>
                  <label> Author  </label>
                 <input
                  type="text" 
                  required
+                 value={author}
+          onChange={(e) => setAuthor(e.target.value)}
                  />
                  <button>Add Book </button>
             </form>
