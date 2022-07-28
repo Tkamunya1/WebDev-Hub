11668b68967a0482c29044e3638d1cdafa5850c2
Titus <titus.kamunya@student.moringaschool.com>
2022-07-28 12:22:42 +0300

    this commit fetches book description from the API

--- src/BookDetails.js
+++ src/BookDetails.js
@@ -2,22 +2,27 @@ import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BookDetails = () => {
-    const { id } = useParams();
-    const { data: blog, error, isPending } = useFetch('https://safe-citadel-50910.herokuapp.com/blogs' + id);
-  
-    return (
-      <div className="blog-details">
-        { isPending && <div>Loading...</div> }
-        { error && <div>{ error }</div> }
-        { blog && (
-          <p>
-            <h2>{ blog.title }</h2>
-            <p>Written by { blog.author }</p>
-            <div>{ blog.body }</div>
-          </p>
-        )}
-      </div>
-    );
-  }
+  const { id } = useParams();
+  const { data: blog, error, isPending } = useFetch('https://safe-citadel-50910.herokuapp.com/blogs' + id );
+
+  return (
+    <div className="book-details">
+      { isPending && <div>Loading...</div> }
+      { error && <div>{ error }</div> }
+      { blog && (
+        <book>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+          <div>{ blog.body }</div>
+        </book>
+      )}
+    </div>
+  );
+}
  
 export default BookDetails;
+
+
+
+
+//https://safe-citadel-50910.herokuapp.com/blogs
