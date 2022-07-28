1c6d0286e87a5bc439b0bf6eac8f4954eab7e5d4
Titus <titus.kamunya@student.moringaschool.com>
2022-07-28 09:01:36 +0300

    this commit will implement useEffect clean up

--- src/BlogList.js
+++ src/BlogList.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const BlogList = ({ blogs }) => {
   return (
-    <div className="blog-list">
+<div className="blog-list">
       {blogs.map(blog => (
         <div className="blog-preview" key={blog.id} >
           <h2>{ blog.title }</h2>
