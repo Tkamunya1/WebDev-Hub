99078f64d49caad73900d54bbe9e01885cd3a3a6
Titus <titus.kamunya@student.moringaschool.com>
2022-07-28 12:21:20 +0300

    this commit will add a form

--- src/Post.js
+++ src/Post.js
@@ -2,6 +2,24 @@ const Post = () => {
     return (
         <div className = "post-book">
             <h1> Post a New Book </h1>
+            <form>
+                <label> Book Title </label>
+                <input
+                 type="text" 
+                 required
+                 />
+                 <label> Book Body  </label>
+                 <textarea 
+                 required
+                 >
+                 </textarea>
+                 <label> Author  </label>
+                <input
+                 type="text" 
+                 required
+                 />
+                 <button>Add Book </button>
+            </form>
             </div>
      );
 }
