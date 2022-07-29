a2ab6f1347be05a0bb85926258c085aca576500e
Titus <titus.kamunya@student.moringaschool.com>
2022-07-29 20:32:34 +0300

    edited out unnecessary details

--- src/BookDetails.js
+++ src/BookDetails.js
@@ -1,20 +1,32 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BookDetails = () => {
   const { id } = useParams();
   const { data: blog, error, isPending } = useFetch('https://safe-citadel-50910.herokuapp.com/blogs' + id );
 
+  const his = useHistory();
+  const handleClick = () => {
+   fetch('https://safe-citadel-50910.herokuapp.com/blogs' + blog.id, {
+     method: 'DELETE'
+   }).then(() => {
+     his.push('/');
+   }) 
+ }
+
   return (
     <div className="book-details">
       { isPending && <div>Loading...</div> }
       { error && <div>{ error }</div> }
       { blog && (
-        <book>
+        <article>
           <h2>{ blog.title }</h2>
+
           <p>Written by { blog.author }</p>
           <div>{ blog.body }</div>
-        </book>
+          {<button> onClick= {handleClick} delete</button> }
+
+        </article>
       )}
     </div>
   );
