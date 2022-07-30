2e7d3ef13c45b16ca0fdba576acbfddff63cba4f
Titus <titus.kamunya@student.moringaschool.com>
2022-07-30 13:07:22 +0300

    delete commented code

--- src/BookList.js
+++ src/BookList.js
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom"
 
 
 const BookList = ({ books }) => {
-  // let BlogList = blogs.map((blog) => (<BlogList title={blog.title} image={blog.image} Id={blog.id} key={blog.id}/>))
  
 
   return (
@@ -10,22 +9,18 @@ const BookList = ({ books }) => {
       {books.map( book => (
         <div className="book-preview" key={book .id} >
 
-          {/* <Link to= { `/blogs/ $ {blog.body}` } >  */}
           <h2>{ book .title }</h2>
           {<img className="image-preview" src={ book .image } alt=""/>}
           <p>Description: { book .body }</p>
           <h5>Written by { book .author } </h5>
           <Link to ={"url"} >Order Book </Link>
 
-          {/* </Link> */}
         </div>
       ))}
     </div>
   );
 }
  
-// export default BlogList ({ image, author, body }) ;
 export default BookList;
 
 
-//          <p>{ blog.body }</p>
